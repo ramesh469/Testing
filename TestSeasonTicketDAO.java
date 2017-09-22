b37f6029ef26ea0b59f586df02bd35d8ed2839e0
ramesh469 <30677263+ramesh469@users.noreply.github.com>
2017-09-22 22:58:19 +1000

    TestTicketDao.java

---
+++ TestSeasonTicketDAO.java
@@ -0,0 +1,60 @@
+package bcccp.test;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import bcccp.tickets.season.SeasonTicketDAO;
+
+public class TestSeasonTicketDAO {
+	
+	static SeasonTicketDAO seasonTicketDAO;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		SeasonTicketDAO.class.getClass();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	@Test
+	public void SeasonTicketDAO() {
+		
+	}
+	@Test
+	public void registerTicket() {
+		
+	}
+	@Test
+	public void deregisterTicket() {
+		
+	}
+	@Test
+	public void getNumberOfTickets() {
+		
+	}
+	@Test
+	public void findTicketById() {
+		
+	}
+	@Test
+	public void recordTicketEntry() {
+		
+	}
+	@Test
+	public void recordTicketExit() {
+		
+	}
+	
+
+}

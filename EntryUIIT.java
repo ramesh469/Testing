6bd8a5065031ec56e04377e3406c7197ced417a8
ramesh469 <30677263+ramesh469@users.noreply.github.com>
2017-09-22 21:48:08 +1000

    Update EntryUIIT.java

--- EntryUIIT.java
+++ EntryUIIT.java
@@ -2,13 +2,40 @@ package bcccp.test;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.Field;
+import java.util.Date;
+
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import bcccp.carpark.entry.EntryController;
+import bcccp.carpark.entry.EntryUI;
+import bcccp.carpark.entry.IEntryController;
 
 public class EntryUIIT {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	static EntryUI entryUI;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		entryUI=new EntryUI(320, 100);
+		entryUI.setVisible(true);
 	}
 
-}
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	

f5dbf3a5a629b9d660de6dc0a99047577c1a666a
ramesh469 <30677263+ramesh469@users.noreply.github.com>
2017-09-22 19:30:48 +1000

    TestRunnerTest.java
    in this test case it will print the ticket

---
+++ TestRunnerTest.java
@@ -0,0 +1,27 @@
+package bcccp.test;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+public class TestRunnerTest {
+
+	@Test
+	public void testPrintTicket() throws Exception{
+		String carparkId="C12345";
+		int tNo=1234;
+		Date entryDate=new Date();
+		Date paidDate=new Date();
+		float charge=123.0f;
+		String barcode="B123456";
+		StringBuilder builder = new StringBuilder();
+		builder.append("Carpark    : " + carparkId + "\n");
+		builder.append("Ticket No  : " + tNo + "\n");
+		builder.append("Entry Time : " + entryDate + "\n");
+		builder.append("Paid  Time : " + paidDate + "\n");
+		builder.append("Charge     : " + String.format("%.2f", charge) + "\n");
+		builder.append("Barcode    : " + barcode + "\n");
+}
+}

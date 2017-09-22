fbfa1aec7e9f90a266835625d1dbe50e809876a1
ramesh469 <30677263+ramesh469@users.noreply.github.com>
2017-09-22 21:27:22 +1000

    ExitUIIT.java

--- ExitUIIT.java
+++ ExitUIIT.java
@@ -2,13 +2,46 @@ package bcccp.test;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.Field;
+
+import javax.swing.JTextField;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import bcccp.carpark.exit.ExitUI;
 
 public class ExitUIIT {
 
+	static ExitUI exitUI;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		exitUI=new ExitUI(1000, 100);
+		exitUI.setVisible(true);
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test()throws Exception {
+		exitUI.display("TCKT123456");
+		Field f = Class.forName("bcccp.carpark.exit.ExitUI").getDeclaredField("displayTextField");
+		f.setAccessible(true);
+		JTextField s = (JTextField) f.get(exitUI);
+		assertEquals(s.getText(),"TCKT123456");
 	}
 
 }

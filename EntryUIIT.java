d3f3b4f3c41c08579c659fa6b6ff5590db425237
ramesh469 <30677263+ramesh469@users.noreply.github.com>
2017-09-22 21:37:00 +1000

    EntryUIIT.java

---
+++ EntryUIIT.java
@@ -0,0 +1,14 @@
+package bcccp.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class EntryUIIT {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

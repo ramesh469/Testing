7be7e925f7e2d37b00560d837cf96149297d23c2
ramesh469 <30677263+ramesh469@users.noreply.github.com>
2017-09-22 22:45:20 +1000

    TestPayStationController.java

---
+++ TestPaystationController.java
@@ -0,0 +1,43 @@
+package bcccp.test;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import bcccp.carpark.paystation.PaystationController;
+
+public class TestPaystationController {
+
+	static PaystationController paystationController;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		PaystationController.class.getClass();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	@Test
+	public void ticketInserted() {
+		
+	}
+	@Test
+	public void ticketPaid() {
+		
+	}
+	@Test
+	public void ticketTaken() {
+		
+	}
+
+}

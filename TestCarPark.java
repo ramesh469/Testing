5b22940bb7fadcd11977d5cfdff22a54cadf0ef0
ramesh469 <30677263+ramesh469@users.noreply.github.com>
2017-09-22 22:48:21 +1000

    TestCarPark.java

---
+++ TestCarPark.java
@@ -0,0 +1,92 @@
+package bcccp.test;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import bcccp.carpark.Carpark;
+
+
+public class TestCarPark {
+
+	static Carpark carPark;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		Carpark.class.getClass();
+	}
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	@Test
+	public void register() {
+		
+	}
+	@Test
+	public void deregister() {
+		
+	}
+	@Test
+	public void getName() {
+		
+	}
+	@Test
+	public void isFull() {
+		
+	}
+	@Test
+	public void issueAdhocTicket() {
+		
+	}
+	@Test
+	public void recordAdhocTicketEntry() {
+		
+	}
+	@Test
+	public void getAdhocTicket() {
+		
+	}
+	@Test
+	public void calculateAddHocTicketCharge() {
+		
+	}
+	@Test
+	public void recordAdhocTicketExit() {
+		
+	}
+	@Test
+	public void registerSeasonTicket() {
+		
+	}
+	@Test
+	public void deregisterSeasonTicket() {
+		
+	}
+	@Test
+	public void isSeasonTicketValid() {
+		
+	}
+	@Test
+	public void isSeasonTicketInUse() {
+		
+	}
+	@Test
+	public void recordSeasonTicketEntry() {
+		
+	}
+	@Test
+	public void recordSeasonTicketExit() {
+		
+	}
+
+
+}
